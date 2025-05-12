2fa32e922eec65194acfb9fef1ff5b3691ce5286
Cyril Rafalovich <replanttrees@gmail.com>
2025-05-12 11:19:50 -1000

    This is the submission for the 1st section lab

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+const userInfo = {
+  username: "user1",
+  password: "password123",
+};
 
+//Login feature
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
 
+  const attemptTracker = (passwordAttempt) => {
+    attemptCount += 1;
+    if (passwordAttempt === userInfo.password) {
+      return "Login successful";
+    } else if (attemptCount > 3) {
+      return `Account locked due to too many failed login attempts`;
+    } else {
+      return `Attempt ${attemptCount}: Login failed`;
+    }
+  };
+
+  return attemptTracker;
+}
 
 module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
+  ...(typeof createLoginTracker !== "undefined" && { createLoginTracker }),
 };
